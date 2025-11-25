aa5dc7362d269fb7ceda0de7f8cd4f351db0039f
Dilan Hosseini <dhosseini.mmt-b2023@fh-salzburg.ac.at>
2025-11-26 00:57:19 +0100

    added precompiled header

--- premake5.lua
+++ premake5.lua
@@ -20,6 +20,9 @@ project "Nika"
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
+	pchheader "nikapch.h"
+	pchsource "Nika/src/nikapch.cpp"
+
 	files
 	{
 		"%{prj.name}/src/**.h",
