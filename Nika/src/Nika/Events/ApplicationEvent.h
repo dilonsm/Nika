7293666a21d79ebc747d72574d61127a0f2def37
Dilan Hosseini <dhosseini.mmt-b2023@fh-salzburg.ac.at>
2025-11-27 17:23:45 +0100

    added events

--- Nika/src/Nika/Events/ApplicationEvent.h
+++ Nika/src/Nika/Events/ApplicationEvent.h
@@ -2,11 +2,9 @@
 
 #include "Event.h"
 
-#include <sstream>
-
 namespace Nika
 {
-	class NIKA_API WindowResizeEvent : public Event 
+	class NIKA_API WindowResizeEvent : public Event
 	{
 	public:
 		WindowResizeEvent(unsigned int width, unsigned int height)
@@ -38,4 +36,32 @@ namespace Nika
 		EVENT_CLASS_TYPE(WindowClose)
 			EVENT_CLASS_CATEGORY(EventCategoryApplication)
 	};
+
+	class NIKA_API AppTickEvent : public Event
+	{
+	public:
+		AppTickEvent() {}
+
+		EVENT_CLASS_TYPE(AppTick)
+			EVENT_CLASS_CATEGORY(EventCategoryApplication)
+	};
+
+	class NIKA_API AppUpdateEvent : public Event
+	{
+	public:
+		AppUpdateEvent() {}
+
+		EVENT_CLASS_TYPE(AppUpdate)
+			EVENT_CLASS_CATEGORY(EventCategoryApplication)
+	};
+
+	class NIKA_API AppRenderEvent : public Event
+	{
+	public:
+		AppRenderEvent() {}
+
+		EVENT_CLASS_TYPE(AppRender)
+			EVENT_CLASS_CATEGORY(EventCategoryApplication)
+	};
 }
+
