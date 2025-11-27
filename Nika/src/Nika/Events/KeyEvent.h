37fc96317de515aac4607b85cc4d9a1a1e0e5eb5
Dilan Hosseini <dhosseini.mmt-b2023@fh-salzburg.ac.at>
2025-11-27 17:24:40 +0100

    minor changes

--- Nika/src/Nika/Events/KeyEvent.h
+++ Nika/src/Nika/Events/KeyEvent.h
@@ -2,8 +2,6 @@
 
 #include "Event.h"
 
-#include <sstream>
-
 namespace Nika
 {
 	class NIKA_API KeyEvent : public Event
--- Nika/src/Nika/Events/MouseEvent.h
+++ Nika/src/Nika/Events/MouseEvent.h
@@ -2,8 +2,6 @@
 
 #include "Event.h"
 
-#include <sstream>
-
 namespace Nika
 {
 	class NIKA_API MouseMovedEvent : public Event
