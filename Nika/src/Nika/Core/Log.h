bab51dee9d3107c2966c4b11c49670590d7994dd
Dilan Hosseini <dilan95@gmx.at>
2025-12-15 22:00:38 +0100

    added debug log level

--- Nika/src/Nika/Core/Log.h
+++ Nika/src/Nika/Core/Log.h
@@ -19,15 +19,17 @@ namespace Nika
 	};
 }
 
-// --- engine-side log macros ---
+// --- engine side log macros ---
 #define NIKA_TRACE(...)			::Nika::Log::getCoreLogger()->trace(__VA_ARGS__)
+#define NIKA_DEBUG(...)			::Nika::Log::getCoreLogger()->debug(__VA_ARGS__)
 #define NIKA_INFO(...)			::Nika::Log::getCoreLogger()->info(__VA_ARGS__)
 #define NIKA_WARN(...)			::Nika::Log::getCoreLogger()->warn(__VA_ARGS__)
 #define NIKA_ERROR(...)			::Nika::Log::getCoreLogger()->error(__VA_ARGS__)
 #define NIKA_CRITICAL(...)		::Nika::Log::getCoreLogger()->critical(__VA_ARGS__)
 
-// --- application-side log macros ---
+// --- application side log macros ---
 #define CLIENT_TRACE(...)		::Nika::Log::getClientLogger()->trace(__VA_ARGS__)
+#define CLIENT_DEBUG(...)		::Nika::Log::getClientLogger()->debug(__VA_ARGS__)
 #define CLIENT_INFO(...)		::Nika::Log::getClientLogger()->info(__VA_ARGS__)
 #define CLIENT_WARN(...)		::Nika::Log::getClientLogger()->warn(__VA_ARGS__)
 #define CLIENT_ERROR(...)		::Nika::Log::getClientLogger()->error(__VA_ARGS__)
